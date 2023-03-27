cf73dba70da3fcc79fb6647be741d6b3bf8d803d
budou114 <58349030+budou114@users.noreply.github.com>
2023-03-27 13:27:26 +0900

    リスト3.8

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 // import App from './App';
 import Hello from './components/Hello';
 import reportWebVitals from './reportWebVitals';
+// import Message from './components/Message';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Hello />
+    {/* <Message /> */}
   </React.StrictMode>
 );
 
